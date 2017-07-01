085372a51b76498bee9d29a5607897ba4ea2689a
cimmanon <chris@cimmanon.org>
2017-07-01 13:56:48 -0400

    Added remaining dfCustom* splices

--- src/Text/Digestive/Heist/Extras.hs
+++ src/Text/Digestive/Heist/Extras.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
 
 module Text.Digestive.Heist.Extras
 	( dfPlainText
@@ -8,15 +8,23 @@ module Text.Digestive.Heist.Extras
 	, dfPlainFile
 
 	, dfCustomText
+	, dfCustomChoice
+	, dfCustomChoiceGroup
+	, dfCustomBool
+	, dfCustomFile
 
 	, dfInputCheckboxMultiple
 	, dfInputListStatic
 	, dfInputListCustom
 	) where
 
+import Control.Exception (SomeException, try)
 import Control.Monad.Trans
 import Data.Map.Syntax (MapSyntax(..), (##))
+import qualified Data.ByteString.Base64 as B64 -- experimental
+import qualified Data.ByteString as BS -- experimental
 import qualified Data.Text as T
+import qualified Data.Text.Encoding as BS -- experimental
 import Text.Digestive.Heist
 import Heist
 import Heist.Interpreted
@@ -26,6 +34,7 @@ import Data.Monoid (mempty)
 import Text.Digestive.Form
 import Text.Digestive.Form.List
 import Text.Digestive.View
+
 
 {----------------------------------------------------------------------------------------------------{
                                                                       | Plain Text
@@ -76,20 +85,105 @@ dfPlainFile view = do
                                                                       | Custom
 }----------------------------------------------------------------------------------------------------}
 
-isDisabled :: MonadIO m => T.Text -> View T.Text -> AttrSplice m
-isDisabled ref view = \_ -> return $
-	if viewDisabled ref view
+-- these splice allow you to create markup exactly the way you want
+
+dfCustomText :: Monad m => View T.Text -> Splice m
+dfCustomText view = do
+	(ref, _) <- getRefAttributes Nothing
+	let
+		attrSplices = "isDisabled" ## isDisabled $ viewDisabled ref view
+	localHS (bindAttributeSplices attrSplices) $ runChildrenWith $ do
+		"name" ## textSplice $ absoluteRef ref view
+		"value" ## textSplice $ fieldInputText ref view
+
+dfCustomChoice :: Monad m => View T.Text -> Splice m
+dfCustomChoice view = do
+	(ref, _) <- getRefAttributes Nothing
+	let
+		choices = fieldInputChoice ref view
+		selected = filter (\(_, _, sel) -> sel) choices
+		attrSplices = "isDisabled" ## isDisabled $ viewDisabled ref view
+	localHS (bindAttributeSplices attrSplices) $ runChildrenWith $ do
+		"name" ## textSplice $ absoluteRef ref view
+		"choice" ## mapSplices choiceSplice choices
+		"selected" ## mapSplices choiceSplice selected
+
+dfCustomChoiceGroup :: Monad m => View T.Text -> Splice m
+dfCustomChoiceGroup view = do
+	(ref, _) <- getRefAttributes Nothing
+	let
+		filterChoice (_, _, sel) = sel
+		choices = fieldInputChoiceGroup ref view
+		selected = filter (\(_, options) -> not $ null $ filter filterChoice options) choices
+		attrSplices = "isDisabled" ## isDisabled $ viewDisabled ref view
+	localHS (bindAttributeSplices attrSplices) $ runChildrenWith $ do
+		"name" ## textSplice $ absoluteRef ref view
+		"choice" ## mapSplices groupSplice choices
+		"selected" ## mapSplices groupSplice selected
+	where
+		groupSplice (name, options) = runChildrenWith $ do
+			"group" ## textSplice name
+			"choice" ## mapSplices choiceSplice options
+
+dfCustomBool :: Monad m => View T.Text -> Splice m
+dfCustomBool view = do
+	(ref, _) <- getRefAttributes Nothing
+	let
+		checked = fieldInputBool ref view
+		attrSplices = do
+			"isDisabled" ## isDisabled $ viewDisabled ref view
+			"isChecked" ## isChecked checked
+	localHS (bindAttributeSplices attrSplices) $ runChildrenWith $ do
+		"name" ## textSplice $ absoluteRef ref view
+		"value" ## textSplice $ if checked then "Yes" else "No"
+
+{-# WARNING dfCustomFile "This splice may or may not work as intended" #-}
+dfCustomFile :: MonadIO m => View T.Text -> Splice m
+dfCustomFile view = do
+	(ref, _) <- getRefAttributes Nothing
+	let
+		attrSplices = "isDisabled" ## isDisabled $ viewDisabled ref view
+	localHS (bindAttributeSplices attrSplices) $ runChildrenWith $ do
+		"name" ## textSplice $ absoluteRef ref view
+		"file" ## mapSplices fileSplice $ fieldInputFile ref view
+	where
+		fileSplice f = runChildrenWith $ do
+			"name" ## textSplice $ T.pack f
+			"data" ## dataSplice f
+
+--------------------------------------------------------------------- | Common Attribute Splices
+
+isDisabled :: Monad m => Bool -> AttrSplice m
+isDisabled x = \_ -> return $
+	if x
 		then [("disabled", "disabled")]
 		else []
 
-dfCustomText :: MonadIO m => View T.Text -> Splice m
-dfCustomText view = do
-	(ref, attrs) <- getRefAttributes Nothing
+isChecked :: Monad m => Bool -> AttrSplice m
+isChecked x = \_ -> return $
+	if x
+		then [("disabled", "disabled")]
+		else []
+
+--------------------------------------------------------------------- | Common Splices
+
+choiceSplice :: Monad m => (T.Text, T.Text, Bool) -> Splice m
+choiceSplice (val, name, sel) =
 	let
-		attrSplices = "isDisabled" ## isDisabled ref view
-	localHS (bindAttributeSplices attrSplices) $ runChildrenWith $ do
-		"value" ## textSplice $ fieldInputText ref view
-		"name" ## textSplice $ absoluteRef ref view
+		attrSplices = do
+			"isChecked" ## isChecked sel
+		splices = do
+			"name" ## textSplice name
+			"value" ## textSplice val
+	in
+		localHS (bindAttributeSplices attrSplices) $ runChildrenWith splices
+
+dataSplice :: MonadIO m => FilePath -> Splice m
+dataSplice path = do
+	contents :: (Either SomeException BS.ByteString) <- liftIO $ try (BS.readFile path)
+	case contents of
+		Right c -> textSplice $ BS.decodeUtf8 $ B64.encode c
+		Left _ -> return []
 
 {----------------------------------------------------------------------------------------------------{
                                                                       | Choice
