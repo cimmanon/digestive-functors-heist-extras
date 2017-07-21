be1e50e7a7825b9c97dc3adcf384b0b882282d6e
cimmanon <chris@cimmanon.org>
2017-07-20 21:42:16 -0400

    Added GroupRadio splices

---
+++ src/Text/Digestive/Heist/Extras/GroupRadio.hs
@@ -0,0 +1,68 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Text.Digestive.Heist.Extras.GroupRadio
+	( dfGroupRadioText
+	, dfGroupRadioChoice
+	) where
+
+import Control.Exception (SomeException, try)
+import Control.Monad.Trans
+import Data.Map.Syntax (MapSyntax(..), (##))
+import Data.Function (on)
+import Data.List (partition, unionBy)
+import qualified Data.Text as T
+import Data.Text (Text)
+import Text.Digestive.Heist
+import Heist
+import Heist.Interpreted
+import qualified Text.XmlHtml as X
+import Data.Monoid (mempty)
+
+import Text.Digestive.Form
+import Text.Digestive.Form.List
+import Text.Digestive.View
+
+----------------------------------------------------------------------
+
+appendElem :: Bool -> a -> [a] -> [a]
+appendElem True x = (x:)
+appendElem False _ = id
+
+addAttrs :: Eq a => [(a, b)] -> [(a, b)] -> [(a, b)]
+addAttrs = unionBy (on (==) fst)
+
+----------------------------------------------------------------------
+
+-- TODO: reduce code duplication here
+
+dfGroupRadioText :: Monad m => View Text -> View Text -> Splice m
+dfGroupRadioText outerView view = do
+	(ref, attrs) <- getRefAttributes Nothing
+	let
+		(confAttrs, attrs') = partition ((== "checked") . fst) attrs
+		value = fieldInputText ref view
+		outerRef = case confAttrs of
+			(("checked", x):_) -> x
+			_ -> error "No field for comparison provided in `checked` attribute"
+		checkedVal = fieldInputText outerRef outerView
+		finalAttrs = addAttrs attrs' $
+			appendElem (viewDisabled ref view) ("disabled", "") $
+			appendElem (value == checkedVal) ("checked", "") $
+			[("type", "radio"), ("id", absoluteRef outerRef view), ("name", absoluteRef outerRef outerView), ("value", value)]
+	return [X.Element "input" finalAttrs []]
+
+dfGroupRadioChoice :: Monad m => View Text -> View Text -> Splice m
+dfGroupRadioChoice outerView view = do
+	(ref, attrs) <- getRefAttributes Nothing
+	let
+		(confAttrs, attrs') = partition ((== "checked") . fst) attrs
+		value = fieldInputText ref view
+		outerRef = case confAttrs of
+			(("checked", x):_) -> x
+			_ -> error "No field for comparison provided in `checked` attribute"
+		selectedVals = map (\(x, _, _) -> x) $ filter (\(_, _, sel) -> sel) $ fieldInputChoice outerRef outerView
+		finalAttrs = addAttrs attrs' $
+			appendElem (viewDisabled ref view) ("disabled", "") $
+			appendElem (value `elem` selectedVals) ("checked", "") $
+			[("type", "radio"), ("id", absoluteRef outerRef view), ("name", absoluteRef outerRef outerView), ("value", value)]
+	return [X.Element "input" finalAttrs []]
