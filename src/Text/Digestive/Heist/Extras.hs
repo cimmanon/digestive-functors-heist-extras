9bbc43d395718ac6149b06fa623e2a19c660aee5
cimmanon <chris@cimmanon.org>
2017-08-09 13:13:01 -0400

    Added copy of dfSubView function from Text.Digestive.Heist that allows you to pass in a list of splices to use in the child nodes

--- src/Text/Digestive/Heist/Extras.hs
+++ src/Text/Digestive/Heist/Extras.hs
@@ -4,13 +4,15 @@ module Text.Digestive.Heist.Extras
 	( module E
 
 	, dfPath
+	, dfSubView
 	, dfInputCheckboxMultiple
 	) where
 
 import Data.Map.Syntax ((##))
-import qualified Data.Text as T
-import Text.Digestive.Heist
-import Text.Digestive.View
+import Control.Monad.Trans (MonadIO)
+import Data.Text (Text)
+import Text.Digestive.View (View, absoluteRef, subView, fieldInputChoice)
+import Heist (Splices)
 import Heist.Interpreted
 import qualified Text.XmlHtml as X
 
@@ -18,21 +20,31 @@ import Text.Digestive.Heist.Extras.Plain as E
 import Text.Digestive.Heist.Extras.Custom as E
 import Text.Digestive.Heist.Extras.List as E
 import Text.Digestive.Heist.Extras.GroupRadio as E
+import Text.Digestive.Heist.Extras.Internal.Attribute (getRefAttributes)
 
 ----------------------------------------------------------------------
 
 -- basic text splice that provides the current path in the form,
 -- handy when you have to manually create some form markup
 -- (eg. form.subview)
-dfPath :: Monad m => View T.Text -> Splice m
+dfPath :: Monad m => View Text -> Splice m
 dfPath view = return [X.TextNode $ absoluteRef "" view]
+
+----------------------------------------------------------------------
+
+-- variation of Text.Digestive.Heist.dfSubView that allows you to pass in a
+-- list of splices to be made accessible in the child nodes
+dfSubView :: MonadIO m => (View Text -> Splices (Splice m)) -> View Text -> Splice m
+dfSubView splices view = do
+	(ref, _) <- getRefAttributes Nothing
+	runChildrenWith $ splices $ subView ref view
 
 ----------------------------------------------------------------------
 
 -- this splice is intended for use with choiceMultiple forms.  it generates a
 -- list of checkboxes, similar to dfInputRadio, but allows for some customization
 {-# DEPRECATED dfInputCheckboxMultiple "Use Text.Digestive.Heist.Extras.Custom.dfCustomChoice instead" #-}
-dfInputCheckboxMultiple :: Monad m => View T.Text -> Splice m
+dfInputCheckboxMultiple :: Monad m => View Text -> Splice m
 dfInputCheckboxMultiple view =  do
 	(ref, _) <- getRefAttributes Nothing
 	let
