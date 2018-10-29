c2866326c193673f591241501d6577ba4560ed1b
cimmanon <chris@cimmanon.org>
2018-10-29 17:24:31 -0400

    Moved dfSubView and dfInputSelectGroup to the Patch module

--- src/Text/Digestive/Heist/Extras.hs
+++ src/Text/Digestive/Heist/Extras.hs
@@ -4,16 +4,12 @@ module Text.Digestive.Heist.Extras
 	( module E
 
 	, dfPath
-	, dfSubView
-	, dfInputSelectGroup
 	, dfInputCheckboxMultiple
 	) where
 
 import Data.Map.Syntax ((##))
-import Control.Monad.Trans (MonadIO)
 import Data.Text (Text)
-import Text.Digestive.View (View, absoluteRef, subView, fieldInputChoice, fieldInputChoiceGroup, viewDisabled)
-import Heist (Splices)
+import Text.Digestive.View (View, absoluteRef, fieldInputChoice)
 import Heist.Interpreted
 import qualified Text.XmlHtml as X
 
@@ -21,7 +17,8 @@ import Text.Digestive.Heist.Extras.Plain as E
 import Text.Digestive.Heist.Extras.Custom as E
 import Text.Digestive.Heist.Extras.List as E (dfInputListStatic, dfInputListCustom, dfInputListSpan)
 import Text.Digestive.Heist.Extras.GroupRadio as E
-import Text.Digestive.Heist.Extras.Internal.Attribute (getRefAttributes, appendAttr, mergeAttrs, disabledAttr)
+import Text.Digestive.Heist.Extras.Patch as E
+import Text.Digestive.Heist.Extras.Internal.Attribute (getRefAttributes)
 
 ----------------------------------------------------------------------
 
@@ -32,33 +29,6 @@ dfPath :: Monad m => View Text -> Splice m
 dfPath view = return [X.TextNode $ absoluteRef "" view]
 
 ----------------------------------------------------------------------
-
--- variation of Text.Digestive.Heist.dfSubView that allows you to pass in a
--- list of splices to be made accessible in the child nodes
-dfSubView :: MonadIO m => (View Text -> Splices (Splice m)) -> View Text -> Splice m
-dfSubView splices view = do
-	(ref, _) <- getRefAttributes Nothing
-	runChildrenWith $ splices $ subView ref view
-
-----------------------------------------------------------------------
-
--- variation of Text.Digestive.Heist.dfSubView.dfInputSelectGroup, gracefully
--- handles `multiple` option for optional results
-dfInputSelectGroup :: Monad m => View Text -> Splice m
-dfInputSelectGroup view = do
-	(ref, attrs) <- getRefAttributes Nothing
-	let
-		ref'     = absoluteRef ref view
-		choices  = fieldInputChoiceGroup ref view
-		kids     = concatMap makeGroup choices
-		finalAttrs = mergeAttrs attrs $ appendAttr (viewDisabled ref view) disabledAttr [("id", ref'), ("name", ref')]
-
-		makeGroup ("", options) = map makeOption options
-		makeGroup (name, options) = [X.Element "optgroup" [("label", name)] $ map makeOption options]
-
-		makeOption (i, c, sel) = X.Element "option" (appendAttr sel ("selected", "selected") [("value", i)]) [X.TextNode c]
-
-	return [X.Element "select" finalAttrs kids]
 
 -- this splice is intended for use with choiceMultiple forms.  it generates a
 -- list of checkboxes, similar to dfInputRadio, but allows for some customization
---
+++ src/Text/Digestive/Heist/Extras/Patch.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Text.Digestive.Heist.Extras.Patch
+	( dfSubView
+	, dfInputSelectGroup
+	) where
+
+import Data.Text (Text)
+import Text.Digestive.View (View, absoluteRef, subView, fieldInputChoiceGroup, viewDisabled)
+import Heist (Splices)
+import Heist.Interpreted
+import qualified Text.XmlHtml as X
+
+import Text.Digestive.Heist.Extras.Internal.Attribute (getRefAttributes, appendAttr, mergeAttrs, disabledAttr)
+
+-- This module contains rewrites of splices found in the standard library.
+
+----------------------------------------------------------------------
+-- variation of Text.Digestive.Heist.dfSubView that allows you to pass in a
+-- list of splices to be made accessible in the child nodes
+dfSubView :: Monad m => (View Text -> Splices (Splice m)) -> View Text -> Splice m
+dfSubView splices view = do
+	(ref, _) <- getRefAttributes Nothing
+	runChildrenWith $ splices $ subView ref view
+
+----------------------------------------------------------------------
+
+-- variation of Text.Digestive.Heist.dfSubView.dfInputSelectGroup, gracefully
+-- handles `multiple` option for optional results
+dfInputSelectGroup :: Monad m => View Text -> Splice m
+dfInputSelectGroup view = do
+	(ref, attrs) <- getRefAttributes Nothing
+	let
+		ref'     = absoluteRef ref view
+		choices  = fieldInputChoiceGroup ref view
+		kids     = concatMap makeGroup choices
+		finalAttrs = mergeAttrs attrs $ appendAttr (viewDisabled ref view) disabledAttr [("id", ref'), ("name", ref')]
+
+		makeGroup ("", options) = map makeOption options
+		makeGroup (name, options) = [X.Element "optgroup" [("label", name)] $ map makeOption options]
+
+		makeOption (i, c, sel) = X.Element "option" (appendAttr sel ("selected", "selected") [("value", i)]) [X.TextNode c]
+
+	return [X.Element "select" finalAttrs kids]
